c56a72f90a970ecfe2e9eb4b2231cc8a93b4897b
Laksh <64250608+Luck-shya@users.noreply.github.com>
2023-10-01 12:25:45 +0530

    Time: 21 ms (24.64%), Space: 13.6 MB (22.36%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ss=s+' ';
+        string a;
+        string ans;
+        for(int i=0;i<ss.size();i++){
+            if(ss[i]==' '){
+                reverse(a.begin(),a.end());
+                ans+=a;
+                ans+=' ';
+                a="";
+            }
+            else{
+                a+=s[i];
+            }
+        }
+        ans=ans.substr(0,ans.size()-1);
+        return ans;
+    }
+};
