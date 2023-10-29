d2acf5d629480e31db8014e444cc14da8c7662b3
Laksh <64250608+Luck-shya@users.noreply.github.com>
2023-10-29 23:36:20 +0530

    Time: 597 ms (47.39%), Space: 10.5 MB (66.22%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2)
+            return 0;
+        
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int p = 2; p * p < n; p++) {
+            if (isPrime[p]) {
+                for (int i = p * p; i < n; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+
+        int count = std::count(isPrime.begin(), isPrime.end(), true);
+        return count;
+    }
+};
