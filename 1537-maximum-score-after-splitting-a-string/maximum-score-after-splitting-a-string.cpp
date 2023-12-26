a6bd4e0bc3c736271c2a0095b560299fea9c0598
Laksh <64250608+Luck-shya@users.noreply.github.com>
2023-12-26 23:17:16 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (52.57%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int maxScore(string s) {
-        vector<int> zero;
-        vector<int> one;
-        for(int i = 1 ;i<s.size();i++){
-            string left = s.substr(0,i);
-            string right = s.substr(i,s.size()-1);
-            int zeros=0,ones=0;
-            for(int j=0;j<left.size();j++){
-                if(left[j]=='0'){
-                    zeros++;
-                }
-            }
-            zero.push_back(zeros);
-            for(int j=0;j<right.size();j++){
-                if(right[j]=='1'){
-                    ones++;
-                }
-            }
-            one.push_back(ones);
-        }
-        int max= 0 ;
-        for(int k=0;k<one.size();k++){
-            int temp = zero[k]+one[k];
-            if(max<temp){
-                max=temp;
+        int zeros = 0, ones = 0;
+        int maxScore = 0;
+        for (char c : s) {
+            if (c == '1') {
+                ones++;
             }
         }
-        return max;
+
+        for (int i = 0; i < s.size() - 1; ++i) {
+            if (s[i] == '0') {
+                zeros++;
+            } else {
+                ones--;
+            }
+            int currentScore = zeros + ones;
+            if (currentScore > maxScore) {
+                maxScore = currentScore;
+            }
+        }
+
+        return maxScore;
     }
 };
