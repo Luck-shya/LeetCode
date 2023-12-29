6af4191eff943ea246d25aa237a00dd25bed4020
Laksh <64250608+Luck-shya@users.noreply.github.com>
2023-12-30 02:29:24 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (40.07%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -1,39 +1,37 @@
-
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
         unordered_map<char, string> aa;
         vector<string> stringsss;
-        string pushi;
-        
-        // Splitting the string into words
-        for (char c : s) {
-            if (c == ' ') {
-                stringsss.push_back(pushi);
-                pushi = "";
-            } else {
-                pushi += c;
+        string word;
+
+        int i = 0;
+        while (i < s.size()) {
+            word.clear();
+            while (i < s.size() && s[i] != ' ') {
+                word += s[i++];
             }
+            stringsss.push_back(word);
+            i++; // Move past the space
         }
-        stringsss.push_back(pushi); // Add the last word
-        
+
         if (pattern.size() != stringsss.size()) {
-            return false; // Patterns and words count mismatch
+            return false; // Mismatch in pattern and word count
         }
-        
+
         for (int i = 0; i < pattern.size(); ++i) {
             char ch = pattern[i];
-            
+
             if (aa.find(ch) == aa.end()) {
-                for (const auto &pair : aa) {
+                for (const auto& pair : aa) {
                     if (pair.second == stringsss[i]) {
-                        return false; 
+                        return false; // Word already mapped to a different character
                     }
                 }
                 aa[ch] = stringsss[i];
             } else {
                 if (aa[ch] != stringsss[i]) {
-                    return false; 
+                    return false; // Character mapped to a different word
                 }
             }
         }
