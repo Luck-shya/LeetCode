ccf645301a18571a207562e62434f9a610362961
Laksh <64250608+Luck-shya@users.noreply.github.com>
2023-12-31 03:17:07 +0530

    Time: 2 ms (65.86%) | Memory: 8 MB (87.22%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        std::unordered_map<int, int> orderMap;
-
-        for (int i = 0; i < arr2.size(); ++i) {
-            orderMap[arr2[i]] = i;
+        vector<int> ans;
+        vector<int> buffer;
+        for(int i=0;i<arr2.size();i++){
+            vector<int>::iterator it = find(arr1.begin(),arr1.end(),arr2[i]);
+            if(it!=arr1.end()){
+                int count = std::count(arr1.begin(), arr1.end(), arr2[i]);
+                for(int j=0;j<count;j++){
+                    ans.push_back(arr2[i]);
+                }
+            }        
         }
-        std::sort(arr1.begin(), arr1.end(), [&](int a, int b) {
-            if (orderMap.find(a) != orderMap.end() && orderMap.find(b) != orderMap.end()) {
-                return orderMap[a] < orderMap[b];
-            } else if (orderMap.find(a) != orderMap.end()) {
-                return true;
-            } else if (orderMap.find(b) != orderMap.end()) {
-                return false;
-            } else {
-                return a < b;
+        for(int j=0;j<arr1.size();j++){
+            vector<int>::iterator it = find(arr2.begin(),arr2.end(),arr1[j]);
+            if(it==arr2.end()){
+                buffer.push_back(arr1[j]);
             }
-        });
-
-        return arr1;
+        }
+        sort(buffer.begin(),buffer.end());
+        ans.insert(ans.end(),buffer.begin(),buffer.end());
+        return ans;
     }
 };
