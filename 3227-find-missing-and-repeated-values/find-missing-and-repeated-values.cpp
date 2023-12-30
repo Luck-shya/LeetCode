465ff5613394f9801b9280d41300732b687ce45b
Laksh <64250608+Luck-shya@users.noreply.github.com>
2023-12-31 03:37:23 +0530

    Time: 11 ms (99.02%) | Memory: 22.7 MB (81.23%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-                int n = grid.size();
-        std::unordered_map<int, int> countMap;
-        std::vector<bool> presentMap(n * n + 1, false); 
-        for (const auto& row : grid) {
-            for (int val : row) {
-                countMap[val]++;
-                presentMap[val] = true;
-                
+       vector<int> res(2,0);
+        int n=grid.size();
+        vector<int> f((n*n)+1,0);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                f[grid[i][j]]++;
             }
         }
-
-        vector<int> ans;
-        for (auto p : countMap) {
-            if (p.second > 1) {
-                ans.push_back(p.first);
-            }
+        for(int i=1;i<f.size();i++)
+        {
+            if(f[i]==2)
+                res[0]=i;
+            if(f[i]==0)
+                res[1]=i;
         }
-
-        for (int i = 1; i <= n * n; ++i) {
-            if (!presentMap[i]) {
-                ans.push_back(i);
-            }
-        }
-
-        return ans;
+        return res;
     }
 };
