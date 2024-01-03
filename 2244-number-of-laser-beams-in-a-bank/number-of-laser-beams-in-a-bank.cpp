f0839eaeb6dc34cba0a2428186893e24f322c753
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-01-03 16:49:40 +0530

    Time: 85 ms (76.99%) | Memory: 29.1 MB (34.88%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -3,6 +3,8 @@ public:
     int numberOfBeams(vector<string>& bank) {
         vector<int> ones;
         int count=0;
+        int prev = 0;
+        int ans=0;
         for(auto s:bank){
             count=0;
             for(auto ch : s){
@@ -10,16 +12,11 @@ public:
                     count++;
                 }
             }
-            if(count>0){
-                ones.push_back(count);
+            if(count!=0){
+                ans+=(prev*count);
+                prev=count;
             }
 
-        }
-        int ans=0;
-        for(int i=1;i<ones.size();i++){
-            if(ones[i]>0){
-                ans+=(ones[i-1]*ones[i]);
-            }
         }
         return ans;
     }
