e36ff3c79a6d3fc6f29e92714bca35f30e64eff7
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-01-06 01:00:19 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (10.66%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         long x=1,p;
-        p=rec(x,n);
+        p=check(x,n);
         return p;
     }
-    bool rec(long k,long n)
+    bool check(long k,long n)
     {
         if(k<n)
-        return rec(k*4,n);
+        return check(k*4,n);
         else if(n==k)
         return 1;
         else
