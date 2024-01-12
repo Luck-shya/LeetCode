aaabda5752927c837dcf6bf6c738ec9669295cbc
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-01-13 00:46:17 +0530

    Time: 5 ms (31.19%) | Memory: 7.1 MB (46.92%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -3,20 +3,19 @@ public:
     bool halvesAreAlike(string s) {
         int cnt = 0,n = s.size();
         transform(s.begin(), s.end(), s.begin(), ::tolower); 
-        for(int  i = 0;i<n;i++)
-        {
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+        for(int  i = 0;i<n;i++){ 
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') {
                 cnt++;
+            }
         }
         int c = 0;
         for(int i = 0;i<n/2;i++)
         {
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
                 c++;
+            }
         }
-        if(2*c==cnt)
-            return true;
-        else
-            return false;
+        if(2*c==cnt) return true;
+        else return false;
     }
 };
