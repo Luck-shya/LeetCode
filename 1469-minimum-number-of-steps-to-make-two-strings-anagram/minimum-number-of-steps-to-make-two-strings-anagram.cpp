5d66cef4590f14a31e9b845488355654f750038b
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-01-13 12:29:11 +0530

    Time: 66 ms (52.94%) | Memory: 17 MB (36.03%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -4,28 +4,16 @@
 class Solution {
 public:
     int minSteps(std::string s, std::string t) {
-        std::unordered_map<char, int> freq;
+        unordered_map<char, int> freq;
 
         for (int i = 0; i < s.size(); i++) {
             freq[s[i]]--;
             freq[t[i]]++;
         }
         int pos=0;
-        int neg=0;
         for(auto it = freq.begin();it!=freq.end();++it){
-            if(it->second>0){
-                pos+=it->second;
-            }
-            else{
-                neg-=it->second;
-            }
+            pos+=abs(it->second);
         }
-        if(pos==neg){
-            return pos;
-        }
-        else{
-            return pos-neg;
-        }
-
+        return pos/2;
     }
 };
