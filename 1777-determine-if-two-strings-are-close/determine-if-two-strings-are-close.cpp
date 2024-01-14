b4d4ab2445899c3e04ef5a60052ea94ec1d5ac26
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-01-14 22:52:59 +0530

    Time: 146 ms (31.18%) | Memory: 21.2 MB (26.20%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) {
+            return false; // Strings must be of equal length to be considered "close"
+        }
+
         unordered_map<char, int> freq1, freq2;
         unordered_set<char> chars1, chars2;
 
@@ -13,18 +17,16 @@ public:
             freq2[c]++;
             chars2.insert(c);
         }
+
+        // Check if the sets of characters are the same
         if (chars1 != chars2) {
             return false;
         }
+
+        // Check if the character frequencies are the same
         vector<int> freqCount1, freqCount2;
-
-        for (auto& pair : freq1) {
-            freqCount1.push_back(pair.second);
-        }
-
-        for (auto& pair : freq2) {
-            freqCount2.push_back(pair.second);
-        }
+        transform(freq1.begin(), freq1.end(), back_inserter(freqCount1), [](const auto& p) { return p.second; });
+        transform(freq2.begin(), freq2.end(), back_inserter(freqCount2), [](const auto& p) { return p.second; });
 
         sort(freqCount1.begin(), freqCount1.end());
         sort(freqCount2.begin(), freqCount2.end());
