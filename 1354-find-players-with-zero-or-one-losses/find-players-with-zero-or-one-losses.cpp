77a2624799f165f089db2610239f130a7cb3ba6e
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-01-15 18:28:43 +0530

    Time: 420 ms (89.64%) | Memory: 162.3 MB (88.28%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,37 +1,31 @@
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         unordered_map<int, int> losses;
-        unordered_set<int> oneLossPlayers;  // To keep track of players with one loss
+        unordered_set<int> oneLossPlayers;  
 
-        for (auto m : matches) {
+        for (auto& m : matches) {
             losses[m[1]]++;
         }
 
         unordered_set<int> zeroLossPlayers;
-        for (auto m : matches) {
+        for (auto& m : matches) {
             if (losses.find(m[0]) == losses.end()) {
                 zeroLossPlayers.insert(m[0]);
             }
         }
 
-        vector<int> a(zeroLossPlayers.begin(), zeroLossPlayers.end());
-        sort(a.begin(),a.end());
-        vector<vector<int>> ans;
-        ans.push_back(a);
-        vector<int> b;
+        vector<int> zeroLossVector(zeroLossPlayers.begin(), zeroLossPlayers.end());
+        sort(zeroLossVector.begin(), zeroLossVector.end());
 
-        for (auto &pair : losses) {
+        vector<int> oneLossVector;
+        for (auto& pair : losses) {
             if (pair.second == 1) {
-                b.push_back(pair.first);  // Players with one loss
+                oneLossVector.push_back(pair.first);  // Players with one loss
             }
         }
-        sort(b.begin(),b.end());
-        ans.push_back(b);
-        return ans;
+        sort(oneLossVector.begin(), oneLossVector.end());
+
+        return {zeroLossVector, oneLossVector};
     }
 };
