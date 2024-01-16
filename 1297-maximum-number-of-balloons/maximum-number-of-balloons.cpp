6d8abdd2712bfd1ee92af33f5d9eb3f79d02c7fb
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-01-17 00:39:25 +0530

    Time: 4 ms (52.62%) | Memory: 6.9 MB (96.35%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,21 +1,20 @@
+#define CHAR_COUNT 26
+
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map<char, int> bl;
-        for (char a : text) {
-            bl[a]++;
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        for (auto& c : text) {
+            switch (c) {
+                case 'b': ++b; break;
+                case 'a': ++a; break;
+                case 'l': ++l; break;
+                case 'o': ++o; break;
+                case 'n': ++n; break;
+            }
         }
-
-        int count = 0;
-        while (bl['a'] >= 1 && bl['b'] >= 1 && bl['n'] >= 1 && bl['l'] >= 2 && bl['o'] >= 2) {
-            bl['a']--;
-            bl['b']--;
-            bl['n']--;
-            bl['l'] -= 2;
-            bl['o'] -= 2;
-            count++;
-        }
-
-        return count;
+        l /= 2;
+        o /= 2;
+        return min({b, a, l, o, n});
     }
 };
