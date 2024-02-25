b3f08e6c69bb17a63d4ad916f1a488b7b3cc92cf
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-02-26 00:44:09 +0530

    Time: 12 ms (20.00%) | Memory: 25.6 MB (20.00%) - LeetSync

--- 3324-split-the-array/split-the-array.cpp
+++ 3324-split-the-array/split-the-array.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
         unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>2){
+        for(auto x : nums){
+            mp[x]++;
+
+        }
+        for(auto x : nums){
+            if(mp[x]>2){
                 return false;
             }
         }
