2f4acd4252ea75d5a9813202493b0f027aae7dd7
Laksh <64250608+Luck-shya@users.noreply.github.com>
2024-02-29 23:56:06 +0530

    Time: 11 ms (43.08%) | Memory: 17.9 MB (59.59%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -6,18 +6,18 @@ public:
         int high=n-1;
         while(low<=high)
         {
-        if(numbers[low]+numbers[high]==target)
-        {
-            return {low+1,high+1};
-        }
-        else if(numbers[low]+numbers[high]>target)
-        {   
-            high--;
-        }
-        else
-        {
-            low++ ;
-        }
+            if(numbers[low]+numbers[high]==target)
+            {
+                return {low+1,high+1};
+            }
+            else if(numbers[low]+numbers[high]>target)
+            {    
+                high--;
+            }
+            else
+            {
+                low++ ;
+            }
         }
         return {};
     }
